632980ea5dd88c05cb09dff3d5aac37d8d6f005f
Sanada <sndtkrh@users.noreply.github.com>
2016-05-20 11:49:00 +0900

    '$'で囲んだものも数式と見なされるようにした

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -1308,7 +1308,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
                         if (toolId != null && ArrayUtils.contains(mathJaxTools, toolId))
                         {
                             // this call to MathJax.Hub.Config seems to be needed for MathJax to work in IE
-                            headJs.append("<script type=\"text/x-mathjax-config\">\nMathJax.Hub.Config({\ntex2jax: { inlineMath: [['\\\\(','\\\\)']] }\n});\n</script>\n");
+                            headJs.append("<script type=\"text/x-mathjax-config\">\nMathJax.Hub.Config({\ntex2jax: { inlineMath: [['\\\\(','\\\\)'], ['$', '$']] }\n});\n</script>\n");
                             headJs.append("<script src=\"").append(MATHJAX_SRC_PATH).append("\"  language=\"JavaScript\" type=\"text/javascript\"></script>\n");
                         }                     
                     }
