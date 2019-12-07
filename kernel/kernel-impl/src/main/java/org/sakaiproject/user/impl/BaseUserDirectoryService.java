47313fc42dab405ff306300621ca79dd406c79d8
Shoji Kajita <kajita@media.kyoto-u.ac.jp>
2019-12-07 13:00:03 +0900

    日本語ロケールの場合は姓名逆転で表示する．また，姓の後のカンマ(,)を空白に変更する．

--- kernel/kernel-impl/src/main/java/org/sakaiproject/user/impl/BaseUserDirectoryService.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/user/impl/BaseUserDirectoryService.java
@@ -2620,13 +2620,23 @@ public abstract class BaseUserDirectoryService implements UserDirectoryService, 
 			if (rv == null)
 			{
 				// or do it this way
+ 				Locale loc = new ResourceLoader().getLocale();
 				StringBuilder buf = new StringBuilder(128);
-				if (m_firstName != null) buf.append(m_firstName);
-				if (m_lastName != null)
-				{
-					if (buf.length() > 0) buf.append(" ");
-					buf.append(m_lastName);
-				}
+ 				if (loc != null && loc.getLanguage().equals("ja"))  {
+ 					if (m_firstName != null) buf.append(m_lastName);
+ 					if (m_lastName != null)
+ 					{
+ 						if (buf.length() > 0) buf.append(" ");
+ 						buf.append(m_firstName);
+ 					}
+ 				} else {
+				    if (m_firstName != null) buf.append(m_firstName);
+				    if (m_lastName != null)
+					{
+					    if (buf.length() > 0) buf.append(" ");
+					    buf.append(m_lastName);
+					}
+ 				}
 
 				if (buf.length() == 0)
 				{
@@ -2733,7 +2743,8 @@ public abstract class BaseUserDirectoryService implements UserDirectoryService, 
 					//KNL-524 no comma if the last name is null
 					if (m_lastName != null)
 					{
-						buf.append(", ");
+					    // buf.append(", ");
+						buf.append(" ");
 					}
 					buf.append(m_firstName);
 				}
