e2590025ceed47c4f3dbfb31180f065bfe463b88
Shoji Kajita <kajita.shoji.5z@kyoto-u.ac.jp>
2023-01-12 11:48:07 +0900

    氏名英語表記対応 #153 roster2モジュール

--- roster2/tool/src/webapp/js/roster.js
+++ roster2/tool/src/webapp/js/roster.js
@@ -824,6 +824,7 @@ Handlebars.registerHelper('translate', function (key) {
 });
 
 Handlebars.registerHelper('getName', function (firstNameLastName) {
+	firstNameLastName = 'true';
   return (firstNameLastName) ? this.displayName : this.sortName;
 });
 
